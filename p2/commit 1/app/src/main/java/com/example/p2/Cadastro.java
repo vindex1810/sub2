7716274d6961d09c68c21f938af32ab77ed82460
vindex1810 <viniciussilva1810@gmail.com>
2020-12-03 20:49:24 -0400

    segundo commit

---
+++ p2/commit 1/app/src/main/java/com/example/p2/Cadastro.java
@@ -0,0 +1,20 @@
+package com.example.p2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void adicionarItem(){
+
+    }
+}
---
+++ p2/commit 1/app/src/main/java/com/example/p2/domain/p2.java
@@ -0,0 +1,25 @@
+package com.example.pokmon.domain;
+
+public class p2 {
+
+    private String ID;
+    private String name;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
